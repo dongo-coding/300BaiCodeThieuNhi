fc65cd5f353de7bea1068ed5fd8658797c6aeb92
Do Ngo <bahuynh619@gmail.com>
2026-06-23 15:03:41 +0700

    Create codeThucTe01.cpp

---
+++ codeThucTe01.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<< (a+1)/2 + (b+1)/2 + (c+1)/2; // số bàn cần mua ít nhất, biết rằng mỗi bàn ngồi được nhiều nhất 2 học sinh
+}
